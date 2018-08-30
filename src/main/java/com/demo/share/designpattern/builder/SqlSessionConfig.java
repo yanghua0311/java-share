3b49e25255e2bde837f96083b1b0c247be6eb29f
yanghua <yanghua@go-goal.com>
2018-08-30 13:59:34 +0800

    add:builder pattern

---
+++ src/main/java/com/demo/share/designpattern/builder/SqlSessionConfig.java
@@ -0,0 +1,14 @@
+package com.demo.share.designpattern.builder;
+
+/**
+ * ..
+ *
+ * @author 杨华
+ * @version 1.0
+ * @company 上海朝阳永续信息技术有限公司
+ * @copyright (c) 2016 SunTime Co'Ltd Inc. All rights reserved.
+ * @date 2018-08-29 17:32
+ * @since JDK1.7
+ */
+public class SqlSessionConfig {
+}

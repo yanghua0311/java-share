e6ddace00619fb38f7e311a0c937b88fc91a24ef
youngwa <youngwa@Admin>
2018-09-08 23:57:22 +0800

    fix:修改文件结构

--- src/main/java/com/demo/share/designpattern/singleton/Test.java
+++ src/main/java/com/demo/share/designpattern/singleton/Test.java
@@ -3,10 +3,15 @@
  */
 package com.demo.share.designpattern.singleton;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * Created by youngwa on 2018/9/05.
  *
  */
 public class Test {
-
+	public static void main(String[] args) {
+		Map<String, String> con = new ConcurrentHashMap<>();
+	}
 }

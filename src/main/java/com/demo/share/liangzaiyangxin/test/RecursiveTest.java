97bb01c50e7998ee911e82a21df9945f341e735a
yanghua <yanghua@go-goal.com>
2018-09-11 09:52:43 +0800

    ...

--- src/main/java/com/demo/share/liangzaiyangxin/test/RecursiveTest.java
+++ src/main/java/com/demo/share/liangzaiyangxin/test/RecursiveTest.java
@@ -1,14 +1,9 @@
 package com.demo.share.liangzaiyangxin.test;
 
 /**
- * �ݹ�...
- *
- * @author ������
+ * @author ssl
  * @version 1.0
- * @company �Ϻ�����������Ϣ�����ɷ����޹�˾
- * @copyright (c) 2018 SunTime Co'Ltd Inc. All rights reserved.
  * @date 2018/9/7 16:22
- * @since JDK1.7
  */
 public class RecursiveTest {
 
@@ -19,12 +14,8 @@ public class RecursiveTest {
     }
 
     /**
-     * @Description: һ����һ��������¥�ݡ����ж���������
-     *
-     * @auther: ������
+     * @Description: 一次爬一个或两个楼梯。共有多少种爬法
      * @date: 16:24 2018/9/7
-     * @param n ¥����
-     * @return: int
      */
     public static int climbStairs(int n) {
         int i = 1;
